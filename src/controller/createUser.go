1aa06fffbb6d509a25546242cd6ea4341ad24587
William-Dias-Marcos <william.diasmarcos@gmail.com>
2024-07-15 07:23:10 -0300

    feat(routes): finalize route configuration for testing and create user object

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,5 +1,24 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
 
-func CreateUser(c *gin.Context){}
+	"github.com/William-Dias-Marcos/Crud-Golang/src/configuration/rest_err"
+	"github.com/William-Dias-Marcos/Crud-Golang/src/controller/model/request"
+	"github.com/gin-gonic/gin"
+)
+
+func CreateUser(c *gin.Context){
+	
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect filds, error=%s\n", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
+}
