6cdaca291fa252984511ba3bf046049361778bd1
William-Dias-Marcos <william.diasmarcos@gmail.com>
2024-07-18 21:54:31 -0300

    feat(api): add improved error messages for API responses

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -3,7 +3,7 @@ package controller
 import (
 	"fmt"
 
-	"github.com/William-Dias-Marcos/Crud-Golang/src/configuration/rest_err"
+	"github.com/William-Dias-Marcos/Crud-Golang/src/configuration/validation"
 	"github.com/William-Dias-Marcos/Crud-Golang/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
@@ -13,9 +13,7 @@ func CreateUser(c *gin.Context){
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := rest_err.NewBadRequestError(
-			fmt.Sprintf("There are some incorrect filds, error=%s\n", err.Error()))
-
+		restErr := validation.ValidateUserError(err)
 		c.JSON(restErr.Code, restErr)
 		return
 	}
